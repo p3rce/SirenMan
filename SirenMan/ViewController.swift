05581fab13501b6b065e7cce234e1f7f9c19a438
Pierce Goulimis <piercemg2013@gmail.com>
2021-03-15 09:45:16 -0400

    Initial Commit

---
+++ SirenMan/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SirenMan
+//
+//  Created by Pierce Goulimis on 2021-03-15.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
